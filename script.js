882999c4db2d873be2977412c2d194a861302759
Festo1 <festoeducation@gmail.com>
2024-10-07 12:06:33 +0300

    Update the hero section vh

--- script.js
+++ script.js
@@ -114,3 +114,29 @@ document.addEventListener("DOMContentLoaded", function () {
 
   observer.observe(counterSection);
 });
+
+// SWIPER SLIDER
+
+var swiper = new Swiper(".portfolio_swiper", {
+  slidesPerView: 1,
+  spaceBetween: 10,
+  loop: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+    },
+    1024: {
+      slidesPerView: 4,
+      spaceBetween: 40,
+    },
+  },
+});
